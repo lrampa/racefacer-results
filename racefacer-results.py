5567ace70a2517286c7c51940023b1b38d77958c
Lukas Rampa <lukas.rampa@centrum.cz>
2024-08-07 21:12:12 +0000

    WIP - db support

--- racefacer-results.py
+++ racefacer-results.py
@@ -1,6 +1,15 @@
 import socketio
 import logging
 import websocket
+import pyodbc
+import os
+
+db_server = os.environ.get('DB_SERVER')
+db_database= os.environ.get('DB_DATABASE')
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+
+conn_str = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={db_server};DATABASE={db_database};UID={db_username};PWD={db_password}'
 
 logging.basicConfig(
     filename="socketio.log",
@@ -29,14 +38,22 @@ def disconnect():
 @sio.event
 def kartarenacheb(data):
     logging.info(f"message received: kartarenacheb - {data}")
-    # sio.emit('my response', {'response': 'my response'})
-    # 
-    # 42["join","kartarenacheb"]
-    # sio.emit('join', 'kartarenacheb')
 
 @sio.on('*')
 def any_event(event, sid, data):
     logging.info(f"event received: {event} - {sid} - {data}")
+
+    data = [
+        ('John', 'Doe', 30),
+        ('Jane', 'Smith', 25)
+    ]
+
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    insert_query = "INSERT INTO YourTableName (FirstName, LastName, Age) VALUES (?, ?, ?)"
+    cursor.executemany(insert_query, data)
+    conn.commit()
+    conn.close()
 
 sio.connect('https://live.racefacer.com:3123/socket.io/')
 sio.wait()
